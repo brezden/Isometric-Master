ed320795d2daba48ba534bc526f3df333cd9af40
Callum Brezden <callum.brezden@clio.com>
2024-02-28 10:43:08 -0800

    Added shape selector

--- Isometric-Master.lua
+++ Isometric-Master.lua
@@ -1,9 +1,8 @@
 local dlg = Dialog("Isometric Master")
-dlg:entry{ id="user_value", label="User Value:", text="Default User" }
-dlg:button{ id="confirm", text="Confirm" }
-dlg:button{ id="cancel", text="Cancel" }
+
+dlg:separator{ text="Shapes" }
+    :button { id="cube", text="Cube", selected=true, onclick=function() dlg:modify{id="cube", selected=true} dlg:modify{id="circle", selected=false} dlg:modify{id="ramp", selected=false} end }
+    :button { id="circle", text="Circle", selected=false, onclick=function() dlg:modify{id="circle", selected=true} dlg:modify{id="cube", selected=false} dlg:modify{id="ramp", selected=false} end }
+    :button { id="ramp", text="Ramp", selected=false, onclick=function() dlg:modify{id="ramp", selected=true} dlg:modify{id="circle", selected=false} dlg:modify{id="cube", selected=false} end }
+
 dlg:show()
-local data = dlg.data
-if data.confirm then
-  app.alert("The given value is '" .. data.user_value .. "'")
-end
